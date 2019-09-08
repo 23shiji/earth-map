14e5c942a6cc0038e6fe10e5ecc760996a8c761f
VIFF <atsunaakiya@outlook.com>
2019-09-08 21:50:34 +1000

    新增地点

--- map/locations/parse.py
+++ map/locations/parse.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import yaml
+import re
 from crypt import crypt
 
 def item(name_zh, name_en, lng, lat, tags, description):
@@ -20,26 +21,30 @@ def item(name_zh, name_en, lng, lat, tags, description):
 
 def parse_pos(s):
     print(s)
-    lat, lng = s.split(', ')
+    lat, lng = re.split(r'[ ,，]+', s)
     return float(lng), -float(lat)
 
 
-def parse_tags(s):
-    return [
+def parse_tags(s, desc):
+    tags = [
         w
         for w in s.split('|')
         if w
     ]
+    if any('首都' in w for w in desc):
+        tags.append('首都')
+    return tags
 
 def parse_description(desc, old):
-    if old != '√':
+    if old != '√' and old != '' and old != '无':
         desc += f"旧称: {old}\n\n"
     return desc
 
 reader = csv.reader(open("cities.csv"))
+next(reader)
 
 data = [
-    item(name_zh, name_en, *parse_pos(position), parse_tags(tags), parse_description(", ".join(description), old_name))
+    item(name_zh, name_en, *parse_pos(position), parse_tags(tags, description), parse_description(", ".join(description), old_name))
     for name_zh, name_en, position, tags, old_name, *description in reader
 ]
 
